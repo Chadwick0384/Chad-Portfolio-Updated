d1b03dba9231ca8a8882acd9150e4d9061bc10ae
ChadHaley0384 <62257389+Chadwick0384@users.noreply.github.com>
2020-09-24 15:14:29 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-list');
+    const navList = document.querySelectorAll('.nav-list li');
+
+    //Toggle Nav
+    burger.addEventListener('click', ()=> {
+        nav.classList.toggle('nav-active');
+
+    //Animate Links
+    navList.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+         } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+    });
+
+    //Burger Animation
+    burger.classList.toggle('toggle');
+
+    });
+}
+
+navSlide();
+
+
+
+
